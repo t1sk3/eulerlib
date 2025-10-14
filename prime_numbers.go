1777c4c096859d023802bbda6a5dcc180477cb68
MARTENS Mattis <mattis.martens@gmail.com>
2025-10-14 12:15:30 +0200

    refactor: simplify ListPrimes function signature and initialization

--- prime_numbers.go
+++ prime_numbers.go
@@ -81,8 +81,7 @@ func ListPrimality[E Integer](n E) []bool {
 }
 
 // Lists all primes up to n
-func ListPrimes[E Integer](n E) []E {
-	res := []E{}
+func ListPrimes[E Integer](n E) (res []E) {
 	p := NewPrimeNumberIterator[E]()
 	for p.Next() <= n {
 		res = append(res, p.Current())
